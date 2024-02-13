c362430d43bb4ee2cb1a657a644d522ebad9d99d
Hestaine <ariffhestaine@gmail.com>
2024-02-13 18:31:15 +0100

    nkb

--- starter/script.js
+++ starter/script.js
@@ -14,73 +14,78 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-console.log(secretNumber)
+console.log(secretNumber);
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '📛 No Number!';
-    score--
-    document.querySelector('.score').textContent = score
-  } else if (guess === secretNumber){
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    // document.querySelector('.message').textContent = '📛 No Number!';
+    displayMessage('📛 No Number!');
+
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
 
     document.querySelector('.number').textContent = secretNumber;
-     
-    document.querySelector('body').style.backgroundColor = '#60b347'
 
-    document.querySelector('.number').style.width = '30rem'
+    document.querySelector('body').style.backgroundColor = '#60b347';
 
-    if(score > highscore){
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if(guess !== secretNumber){
-  if (score > 1) {
-        document.querySelector('.message').textContent = guess > secretNumber? '📈 Too High!': '📉 Too Low!';
-        score-- 
-        document.querySelector('.score').textContent = score
-      } else{
-        document.querySelector('.message').textContent = '💥 You lost the game';
-        document.querySelector('.score').textContent = score
-      }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game');
+      document.querySelector('.score').textContent = score;
+    }
   } // else if(guess > secretNumber){
   //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too Low!';
-  //     score-- 
+  //     displayMessage('📈 Too Low!');
+  //     score--
   //     document.querySelector('.score').textContent = score
   //   } else{
-  //     document.querySelector('.message').textContent = '💥 You lost the game';
+  //     displayMessage('💥 You lost the game');
   //     document.querySelector('.score').textContent = score
   //   }
   // } else if (guess < secretNumber){
   //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📉 Too Low!';
+  //     displayMessage('📉 Too Low!');
   //     score--
   //     document.querySelector('.score').textContent = score
   //   } else{
-  //     document.querySelector('.message').textContent = '💥 You lost the game';
+  //     displayMessage('💥 You lost the game');
   //     document.querySelector('.score').textContent = score
   //   }
   // }
 });
 
-document.querySelector('.again').addEventListener('click', function(){
-  score = 20
-  document.querySelector('.score').textContent = score
-  
-  document.querySelector('.message').textContent = 'Start guessing...';
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  displayMessage('Start guessing...');
 
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  
+
   document.querySelector('.number').textContent = '?';
 
   document.querySelector('.guess').value = '';
-  
+
   document.querySelector('.number').style.width = '15rem';
-  
-  document.querySelector('body').style.backgroundColor = '#222'
-  console.log(secretNumber)
-})
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  console.log(secretNumber);
+});
